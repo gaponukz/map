838c5ffc0e06f5dc21105a19e7951b31e8f8750b
gaponukz <gaponukz54@gmail.com>
2022-09-22 02:23:50 +0300

    Update

--- utils.py
+++ utils.py
@@ -17,19 +17,25 @@ class Point(object):
 
         # 6371 - radius of earth in kilometers
         return 2 * 6371 * math.asin(math.sqrt(a))
+    
+    def __str__(self):
+        return f"{self.__class__.__name__}(radius={round(self.lat, 3)}, {round(self.lng, 3)})"
 
 class Circle(object):
     def __init__(self, radius: float, center: Point):
         self.radius = radius
         self.center = center if isinstance(center, Point) else Point(*center)
     
-    def has_intersections_with(self, circle):
+    def has_intersections_with(self, circle: 'Circle'):
         max_dist = self.radius + circle.radius
         actual_dist = self.center.distance_to(circle.center)
 
         return actual_dist <= max_dist
 
-    def has_inside(self, circle):
+    def has_inside(self, circle: 'Circle'):
         between_centers = self.center.distance_to(circle.center)
 
         return between_centers + circle.radius <= self.radius
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(radius={round(self.radius, 3)})"
