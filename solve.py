3d2de0db9dfc3aa93d2c9b643f7efca5ca0673b9
gaponukz <gaponukz54@gmail.com>
2022-09-22 01:00:37 +0300

    Add utils file

--- solve.py
+++ solve.py
@@ -1,20 +1,6 @@
-import math
+from utils import *
 
-Point = tuple[float, float]
+point1 = Point(53.32055555555556, -1.7297222222222221)
+point2 = Point(53.31861111111111, -1.6997222222222223)
 
-def distance(point1: Point, point2: Point) -> float:
-    # calculate Distance Between Two Points on Earth
-
-    lat1, lon1 = math.radians(point1[0]), math.radians(point1[1])
-    lat2, lon2 = math.radians(point2[0]), math.radians(point2[1])
-
-    a = math.sin((lat2 - lat1) / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin((lon2 - lon1) / 2)**2
-
-    # 6371 - radius of earth in kilometers
-    return 2 * 6371 * math.asin(math.sqrt(a))
-
-if __name__ == '__main__':
-    point1 = (53.32055555555556, -1.7297222222222221)
-    point2 = (53.31861111111111, -1.6997222222222223)
-
-    print(distance(point1, point2), "K.M")
+print(point1.distance_to(point2), "K.M")
---
+++ utils.py
@@ -0,0 +1,35 @@
+import math
+
+class Point(object):
+    def __init__(self, lat: float, lng: float):
+        self.lat = lat
+        self.lng = lng
+    
+    def distance_to(self, point) -> float:
+        # calculate Distance Between Two Points on Earth
+
+        point = point if isinstance(point, Point) else Point(*point)
+
+        lat1, lon1 = math.radians(self.lat), math.radians(self.lng)
+        lat2, lon2 = math.radians(point.lat), math.radians(point.lng)
+
+        a = math.sin((lat2 - lat1) / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin((lon2 - lon1) / 2)**2
+
+        # 6371 - radius of earth in kilometers
+        return 2 * 6371 * math.asin(math.sqrt(a))
+
+class Circle(object):
+    def __init__(self, radius: float, center: Point):
+        self.radius = radius
+        self.center = center if isinstance(center, Point) else Point(*center)
+    
+    def has_intersections_with(self, circle):
+        max_dist = self.radius + circle.radius
+        actual_dist = self.center.distance_to(circle.center)
+
+        return actual_dist <= max_dist
+
+    def has_inside(self, circle):
+        between_centers = self.center.distance_to(circle.center)
+
+        return between_centers + circle.radius <= self.radius
