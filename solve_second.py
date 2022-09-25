18df5ee3c7aac25ecbc6778b61b17efc59defaa0
gaponukz <gaponukz54@gmail.com>
2022-09-25 03:49:56 +0300

    Find better algorithm

--- solve_second.py
+++ solve_second.py
@@ -2,7 +2,7 @@ import json
 import time
 from utils import *
 
-E = 1 # constant of angle
+E = 2 # constant of angle
 
 with open("setting.json", 'r', encoding='utf-8') as out:
     setting = json.load(out)
@@ -16,16 +16,24 @@ start_time = time.time()
 main_circle = Circle(big_radius, (lat, lng))
 
 circles: list[Circle] = []
-current_circle = main_circle.build_inside_circle_on_angel(0, little_radius)
-circles.append(current_circle)
+
+for angel in range(0, 330, E):
+    new_circle = main_circle.build_inside_circle_on_angel(angel, little_radius)
+
+    if main_circle.has_inside(new_circle):
+        if all([not _circle.has_intersections_with(new_circle) for _circle in circles if new_circle != _circle]):
+            circles.append(new_circle)
+
+print(len(circles))
 
 for circle in circles:
-    for angel in range(0, 360, E):
+    for angel in range(60, 330, E):
         new_circle = circle.build_neighboring_circle_on_angel(angel)
 
         if main_circle.has_inside(new_circle):
             if all([not _circle.has_intersections_with(new_circle) for _circle in circles if new_circle != _circle]):
                 circles.append(new_circle)
+                break
     
     print(len(circles))
 
